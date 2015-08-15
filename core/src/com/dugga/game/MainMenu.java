4a7ee44e95fa462a4a4381f9f9b6ea58c6864128
kentjhall <kentjhall3@hotmail.com>
2015-08-15 13:33:23 -0400

    2nd umpteenth push.

--- core/src/com/dugga/game/MainMenu.java
+++ core/src/com/dugga/game/MainMenu.java
@@ -2,11 +2,13 @@ package com.dugga.game;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.utils.Timer;
 
 /**
  * Created by kenthall on 8/14/15.
@@ -26,6 +28,7 @@ public class MainMenu {
     private Texture upArrow;
     private Texture downArrow;
     private Texture check;
+    private Pixmap arrowPix;
     private BitmapFont titleFont;
     private FreeTypeFontGenerator generator;
     private FreeTypeFontGenerator.FreeTypeFontParameter titleParameter;
@@ -47,6 +50,8 @@ public class MainMenu {
         downArrow=new Texture("downArrow.png");
         check=new Texture("check.png");
         generator=new FreeTypeFontGenerator(Gdx.files.internal("font.ttf"));
+        arrowPix=new Pixmap((int) (Gdx.graphics.getWidth() / 2 - (phoneWidth * 0.8) / 2), 200, Pixmap.Format.Alpha);
+        arrowPix.setColor(Color.CLEAR);
 
         titleLayout=new GlyphLayout();
         titleParameter=new FreeTypeFontGenerator.FreeTypeFontParameter();
@@ -55,12 +60,17 @@ public class MainMenu {
         titleFont=generator.generateFont(titleParameter);
     }
 
-    public void draw(SpriteBatch batch){
+    public void draw(final SpriteBatch batch){
         MyGdxGame.setPlayer(new Player(Gdx.graphics.getWidth() / 2 + (int)MyGdxGame.getPlayer().getWidth()/2, Gdx.graphics.getHeight() / 2+ (int)MyGdxGame.getPlayer().getHeight()/2));
         batch.draw(bg, 0, 0, width, height);
         batch.draw(circleOutline, MyGdxGame.getPlayer().getLocPlayer().x-300/2, MyGdxGame.getPlayer().getLocPlayer().y-300/2, 300, 300);
         batch.draw(circle, MyGdxGame.getPlayer().getLocPlayer().x-(float)circleWidth/2, MyGdxGame.getPlayer().getLocPlayer().y-(float)circleHeight/2, (float) circleWidth, (float) circleHeight);
         batch.draw(phone, Gdx.graphics.getWidth()/2-phoneWidth/2, 100, phoneWidth, phoneHeight);
+        Timer.schedule(new Timer.Task() {
+            @Override
+            public void run() {
+            }
+        }, 0, (float) 0.25);
 
         titleLayout.setText(titleFont, "NAME");
         titleFont.draw(batch, "NAME", Gdx.graphics.getWidth() / 2 - titleLayout.width / 2, Gdx.graphics.getHeight() - titleLayout.height / 2);
@@ -74,6 +84,13 @@ public class MainMenu {
             }
         }
         else{
+            if ((int)Gdx.input.getAccelerometerY()>1){
+                batch.draw(upArrow, Gdx.graphics.getWidth() / 2 - (float) (phoneWidth * 0.8) / 2, 200, (float) (phoneWidth * 0.8), (float) (phoneWidth * 0.8));
+            }
+            else if ((int)Gdx.input.getAccelerometerY()<-1){
+                batch.draw(downArrow, Gdx.graphics.getWidth() / 2 - (float) (phoneWidth * 0.8) / 2, 200, (float) (phoneWidth * 0.8), (float) (phoneWidth * 0.8));
+            }
+
             if (circleWidth>0 && circleHeight>0){
                 circleWidth-=5;
                 circleHeight-=5;
