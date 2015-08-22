aa3a55f02c571e5eb890c09febe26d9bc53d6e8a
kentjhall <kentjhall3@hotmail.com>
2015-08-22 02:20:33 -0400

    more progress with ads

--- android/src/com/dugga/game/android/AndroidLauncher.java
+++ android/src/com/dugga/game/android/AndroidLauncher.java
@@ -1,16 +1,43 @@
 package com.dugga.game.android;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.RelativeLayout;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.dugga.game.MyGdxGame;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new MyGdxGame(), config);
+
+		RelativeLayout layout=new RelativeLayout(this);
+
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+		View gameView=initializeForView(new MyGdxGame());
+
+		AdView adView = new AdView(this);
+		adView.setAdUnitId("ca-app-pub-4743789296025031/9278350238");
+		adView.setAdSize(AdSize.BANNER);
+		adView.loadAd(new AdRequest.Builder().addTestDevice("3BF4804197206FBDAF75FED41AC53782").build());
+
+		RelativeLayout.LayoutParams adParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+		adParams.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+		adParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+
+		layout.addView(gameView);
+		layout.addView(adView, adParams);
+		setContentView(layout);
 	}
 }
